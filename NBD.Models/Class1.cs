0c5dd4951ddd4ee2d778b3c09884ee0568fcf320
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-06-18 20:49:28 +0300

    Initial commit. Added Goal.cs and Docker. Created NBD.Tracker.

--- NBD.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NBD.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ NBD.SDK/Goal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NBD.SDK
+{
+    public abstract class Goal
+    {
+        private Guid id;
+        private DateTime startTime;
+
+        public Goal(string title, DateTime startTime, DateTime endTime)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentException("Title must not be empty.");
+            }
+
+            if (startTime <= DateTime.Now)
+            {
+                throw new ArgumentException("Start time must be in the future.");
+            }
+
+            if (endTime <= DateTime.Now || endTime <= startTime)
+            {
+                throw new ArgumentException("End time must be greater than start time.");
+            }
+
+            this.Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; }
+
+        public string Title { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+    }
+}
