1f36d734807a14bd63371c8e88c5a1d35cbde420
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-06-19 22:17:14 +0300

    Added validation, Target, and AddSubGoal().

--- NBD.SDK/Goal.cs
+++ NBD.SDK/Goal.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NBD.SDK
 {
-    public abstract class Goal
+    public class Goal
     {
         private Guid id;
         private string title;
@@ -10,8 +11,15 @@ namespace NBD.SDK
         private DateTime endDate;
         private Recurrence recurrence;
         private Progress progress;
+        private int? target;
+        private ICollection<Goal> subGoals;
 
-        public Goal(string title, DateTime startDate, DateTime endDate, Recurrence recurrence)
+        public Goal(
+            string title, 
+            DateTime startDate, 
+            DateTime endDate, 
+            Recurrence recurrence, 
+            int? target = null)
         {
             if (string.IsNullOrEmpty(title))
             {
@@ -28,24 +36,54 @@ namespace NBD.SDK
                 throw new ArgumentException("End time must be greater than start time.");
             }
 
+            if (recurrence.Type != RecurrenceType.None && recurrence.Value == 0)
+            {
+                throw new ArgumentException(
+                    "Recurrence value must be greater than 0 when there is a recurrency type.");
+            }
+
             this.id = Guid.NewGuid();
             this.title = title;
             this.startDate = startDate;
             this.endDate = endDate;
-            this.recurrence = recurrence; // TODO VALIDATE
-            this.progress = new Progress(this);
+            this.recurrence = recurrence;
+            this.progress = new Progress(this.recurrence);
+            this.subGoals = new List<Goal>();
+            this.target = target;
         }
 
         public Guid Id => this.id;
 
         public string Title => this.title;
 
-        public DateTime StartTime => this.startDate;
+        public DateTime StartDate => this.startDate;
 
-        public DateTime EndTime => this.endDate;
+        public DateTime EndDate => this.endDate;
 
         public Progress Progress => this.progress;
 
-        public abstract void MakeProgress();
+        public Recurrence Recurrence => this.recurrence;
+
+        public int? Target => this.target;
+
+        public void MakeProgress()
+        {
+            this.progress.MakeProgress();
+        }
+
+        public void AddSubGoal(Goal goal)
+        {
+            if (goal.StartDate <= this.StartDate || goal.StartDate >= this.EndDate)
+            {
+                throw new ArgumentException("Invalid subgoal start date.");
+            }
+
+            if (goal.EndDate <= this.StartDate || goal.EndDate >= this.EndDate)
+            {
+                throw new ArgumentException("Invalid subgoal end date.");
+            }
+
+            this.subGoals.Add(goal);
+        }
     }
 }
