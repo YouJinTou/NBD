6098759116b616a4095ce49d51d052c9dce1b213
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-06-20 23:12:48 +0300

    Simplified Goal.

--- NBD.SDK/Goal.cs
+++ NBD.SDK/Goal.cs
@@ -3,22 +3,20 @@ using System.Collections.Generic;
 
 namespace NBD.SDK
 {
-    public class Goal
+    public abstract class Goal
     {
         private Guid id;
         private string title;
         private DateTime startDate;
         private DateTime endDate;
-        private Recurrence recurrence;
-        private Progress progress;
         private int? target;
+        private int progress;
         private ICollection<Goal> subGoals;
 
         public Goal(
             string title, 
             DateTime startDate, 
             DateTime endDate, 
-            Recurrence recurrence, 
             int? target = null)
         {
             if (string.IsNullOrEmpty(title))
@@ -36,18 +34,10 @@ namespace NBD.SDK
                 throw new ArgumentException("End time must be greater than start time.");
             }
 
-            if (recurrence.Type != RecurrenceType.None && recurrence.Value == 0)
-            {
-                throw new ArgumentException(
-                    "Recurrence value must be greater than 0 when there is a recurrency type.");
-            }
-
             this.id = Guid.NewGuid();
             this.title = title;
             this.startDate = startDate;
             this.endDate = endDate;
-            this.recurrence = recurrence;
-            this.progress = new Progress(this.recurrence);
             this.subGoals = new List<Goal>();
             this.target = target;
         }
@@ -60,15 +50,15 @@ namespace NBD.SDK
 
         public DateTime EndDate => this.endDate;
 
-        public Progress Progress => this.progress;
-
-        public Recurrence Recurrence => this.recurrence;
-
         public int? Target => this.target;
 
-        public void MakeProgress()
+        public bool IsFinished => 
+            (this.progress >= target) || 
+            (this.progress > 0 && target == null);
+
+        public void MakeProgress(int chunk)
         {
-            this.progress.MakeProgress();
+            this.progress += chunk;
         }
 
         public void AddSubGoal(Goal goal)
