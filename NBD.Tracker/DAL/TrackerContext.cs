70701c9ad413a2cf338dcca32fdf5c843a1c95fc
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-06-24 18:08:24 +0300

    Added TrackerContext.

---
+++ NBD.Tracker/DAL/TrackerContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NBD.SDK;
+
+namespace NBD.Tracker.DAL
+{
+    public class TrackerContext : DbContext
+    {
+        public DbSet<Goal> Goals { get; set; }
+    }
+}
