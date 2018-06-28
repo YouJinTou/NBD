e11b99f590ebf8030b99408bceeeb97ed9713ee5
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-06-28 07:15:24 +0300

    Added DbContextOptions<> to the constructor.

--- NBD.Tracker/DAL/TrackerContext.cs
+++ NBD.Tracker/DAL/TrackerContext.cs
@@ -5,6 +5,11 @@ namespace NBD.Tracker.DAL
 {
     public class TrackerContext : DbContext
     {
+        public TrackerContext(DbContextOptions<TrackerContext> options)
+            : base(options)
+        {
+        }
+
         public DbSet<Goal> Goals { get; set; }
     }
 }
