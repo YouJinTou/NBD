0f496c902001754428dbcef37c1963af98d4d9fe
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-07-01 22:47:16 +0300

    Added FutureDateTimeAttribute.cs.

---
+++ NBD.SDK/Attributes/FutureDateTimeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NBD.SDK.Attributes
+{
+    public class FutureDateTimeAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var date = (DateTime?)value;
+
+            if (date != null)
+            {
+                return date.Value <= DateTime.Now ?
+                    new ValidationResult("Date must be in the future.") :
+                    ValidationResult.Success;
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
