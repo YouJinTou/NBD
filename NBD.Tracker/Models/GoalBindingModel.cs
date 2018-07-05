e3a95ec494cda62b973b688ce7902c829fcab4ec
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-07-05 18:40:08 +0300

    Added GoalBindingModel.

---
+++ NBD.Tracker/Models/GoalBindingModel.cs
@@ -0,0 +1,24 @@
+﻿using NBD.SDK;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NBD.Tracker.Models
+{
+    public class GoalBindingModel
+    {
+        public Guid? ParentGoalId { get; set; }
+
+        [Required(ErrorMessage = "The title is required.")]
+        public string Title { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public RecurrenceType RecurrenceType { get; set; }
+
+        public uint RecurrenceValue { get; set; }
+
+        public int? Target { get; set; }
+    }
+}
