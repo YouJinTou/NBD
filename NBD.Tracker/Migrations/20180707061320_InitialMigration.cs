77151f30e878ba039d3f702ee3336fbb4585916c
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-07-07 09:20:38 +0300

    Added an initial migration.

---
+++ NBD.Tracker/Migrations/20180707061320_InitialMigration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NBD.Tracker.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Goals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    ParentGoalId = table.Column<Guid>(nullable: true),
+                    Title = table.Column<string>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: true),
+                    EndDate = table.Column<DateTime>(nullable: true),
+                    RecurrenceType = table.Column<int>(nullable: false),
+                    RecurrenceValue = table.Column<long>(nullable: false),
+                    Target = table.Column<int>(nullable: true),
+                    Progress = table.Column<long>(nullable: false),
+                    IsReached = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Goals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Goals_Goals_ParentGoalId",
+                        column: x => x.ParentGoalId,
+                        principalTable: "Goals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Goals_ParentGoalId",
+                table: "Goals",
+                column: "ParentGoalId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Goals");
+        }
+    }
+}
