7bdf5fdca35d062903e3cf1040e0134c26a69ae2
Dimitar Dimitrov <dandreevd@gmail.com>
2018-07-13 11:33:12 +0300

    Added onSubmit().

--- NBD.App/src/app/goals/goal-form/goal-form.component.ts
+++ NBD.App/src/app/goals/goal-form/goal-form.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+import { GoalsService } from '../goals.service';
+import { Goal } from '../goal';
+
 @Component({
     selector: 'goal-form',
     templateUrl: './goal-form.component.html',
     styleUrls: ['./goal-form.component.css']
 })
 export class GoalFormComponent {
+    goal: Goal;
+
+    constructor(private goalsService: GoalsService) { 
+        this.goal = new Goal();
+    }
+
+    onSubmit() {
+        this.goalsService.addGoal(this.goal);
+    }
 }
