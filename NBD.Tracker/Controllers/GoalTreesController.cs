f0f688c0a05a5f14fdd252c9736a84b3f692530d
Dimitar Dimitrov <dandreevd@gmail.com>
2018-07-13 15:55:35 +0300

    Added GoalTreesController.

---
+++ NBD.Tracker/Controllers/GoalTreesController.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using NBD.SDK;
+using NBD.Tracker.DAL;
+using NBD.Tracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NBD.Tracker.Controllers
+{
+    [EnableCors("UIPolicy")]
+    [Route("api/[controller]")]
+    public class GoalTreesController : Controller
+    {
+        private readonly IRepository<GoalTree> trees;
+        private readonly IRepository<Goal> goals;
+
+        public GoalTreesController(IRepository<GoalTree> trees, IRepository<Goal> goals)
+        {
+            this.trees = trees;
+            this.goals = goals;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetTreeAsync(Guid id)
+        {
+            try
+            {
+                var tree = await this.trees.GetAsync(id);
+                var goals = this.goals.Where(g => g.RootId == tree.RootId);
+                var model = Mapper.Map<IEnumerable<Goal>, IEnumerable<GoalViewModel>>(goals);
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> CreateTreeAsync([FromBody]GoalTreeBindingModel model)
+        {
+            try
+            {
+                var goal = Mapper.Map<GoalBindingModel, Goal>(model.RootGoal);
+                var tree = Mapper.Map<GoalTreeBindingModel, GoalTree>(model);
+                goal.RootId = goal.Id;
+                tree.RootId = goal.Id;
+
+                await this.goals.AddAsync(goal);
+
+                await this.trees.AddAsync(tree);
+
+                return Ok(tree);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+    }
+}
