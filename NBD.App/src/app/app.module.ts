a2f34ebcd0f0f57051efbe5e0b3299ee9395791f
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-07-13 06:38:28 +0300

    Began working on goal-form.component.

--- NBD.App/src/app/app.module.ts
+++ NBD.App/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { GoalFormComponent } from './goals/goal-form/goal-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ NBD.App/src/app/goals/goal-form/goal-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'goal-form',
+    templateUrl: './goal-form.component.html',
+    styleUrls: ['./goal-form.component.css']
+})
+export class GoalFormComponent {
+}
