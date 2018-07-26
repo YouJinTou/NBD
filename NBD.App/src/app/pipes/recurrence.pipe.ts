45aa6112bd451f5dca8a082c6a7d46492c71fd81
Dimitar Dimitrov <dandreevd@gmail.com>
2018-07-26 10:12:32 +0300

    Added parentheses for proper rendering.

--- NBD.App/src/app/pipes/recurrence.pipe.ts
+++ NBD.App/src/app/pipes/recurrence.pipe.ts
@@ -7,10 +7,10 @@ export class RecurrencePipe implements PipeTransform {
 
         switch (type) {
             case 0: return "Once";
-            case 1: return "Every " + isPlural ? value + " days" : "day";
-            case 2: return "Every " + isPlural ? value + " weeks" : "week";
-            case 3: return "Every " + isPlural ? value + " months" : "month";
-            case 4: return "Every " + isPlural ? value + " years" : "year";
+            case 1: return "Every " + (isPlural ? value + " days" : "day");
+            case 2: return "Every " + (isPlural ? value + " weeks" : "week");
+            case 3: return "Every " + (isPlural ? value + " months" : "month");
+            case 4: return "Every " + (isPlural ? value + " years" : "year");
         }
     }
 }
