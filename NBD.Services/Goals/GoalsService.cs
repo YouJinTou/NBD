03a5876b5eba8baff7ceb74cbf1d80be448da005
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-10-13 11:54:25 +0300

    Added NBD.Services.

---
+++ NBD.Services/Goals/GoalsService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using NBD.SDK;
+using NBD.Services.Core.Exceptions;
+
+namespace NBD.Services.Goals
+{
+    public class GoalsService : IGoalsService
+    {
+        //private readonly IRepository<Goal> goals;
+
+        public GoalsService()
+        {
+
+        }
+
+        public Task<Goal> GetGoalAsync(Guid id)
+        {
+            throw new NotImplementedException();
+            //var goal = this.goals.Where(g => g.Id == id).FirstOrDefault();
+
+            //if (goal == null)
+            //{
+            //    throw new GoalNotFoundException(id.ToString());
+            //}
+
+            //return goal;
+        }
+    }
+}
---
+++ NBD.Services/Goals/IGoalsService.cs
@@ -0,0 +1,11 @@
+﻿using NBD.SDK;
+using System;
+using System.Threading.Tasks;
+
+namespace NBD.Services.Goals
+{
+    public interface IGoalsService
+    {
+        Task<Goal> GetGoalAsync(Guid id);
+    }
+}
